3ff1c0534e3f0db29f32c2a1e2c069825269939f
unknown <philipok4596@gmail.com>
2018-03-11 20:31:52 +0300

    longest-consecutive-sequence/feature: implement longestConsecutiveLength function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let savedLength = 0, tempLength = 1;
+  array = array.sort((a, b) => {return a - b;});
+  let savedNum = -1;
+  for (num of array){
+    if (num === (savedNum + 1)) {savedNum++; tempLength++;}
+    else if (num === savedNum) continue;
+    else {savedNum = num; if (savedLength < tempLength) savedLength = tempLength; tempLength = 1}
+  }
+  return savedLength;
 }
